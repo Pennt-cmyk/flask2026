42a3dd478d566b8400c66f7da3a67d1b479906ff
Pennt-cmyk <penny0935555784@gmail.com>
2026-03-26 11:21:02 +0800

    vercel_v0

--- web.py
+++ web.py
@@ -9,11 +9,13 @@ def index():
     link += "<a href=/mis>課程</a><hr>"
     link += "<a href=/today>現在日期時間</a><hr>"
     link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=珮芹&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
+    link += "<a href=/account>網頁表單傳值</a><hr>"
     return link
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1>"
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
 
 @app.route("/today")
 def today():
@@ -26,9 +28,20 @@ def me():
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
-    user = request.values.get("nick")
-    return render_template("welcome.html", name=user)
+    user = request.values.get("u")
+    d = request.values.get("d")
+    c = request.values.get("c")
+    return render_template("welcome.html", name = user,dep = d,course =c)
 
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
 
 
 
