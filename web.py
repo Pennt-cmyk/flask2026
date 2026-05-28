7facab1cb2ae26ef07d0a6f9180df1baef0d5843
Pennt-cmyk <penny0935555784@gmail.com>
2026-05-28 10:44:41 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
+from google import genai
 
 import os
 import json
@@ -22,6 +23,9 @@ firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
+client = genai.Client()
+
+
 @app.route("/")
 def index():
     link = "<h1>歡迎進入林珮芹的網站</h1>"
@@ -41,6 +45,16 @@ def index():
     link += "<br><a href=/weather>天氣</a><hr>"
     link += "<br><a href=/rate>本週新片</a><hr>"
     return link
+
+@app.route("/AI")
+def AI():
+    # 每次使用者拜訪該路徑時，直接使用全域的 client 呼叫模型
+    response = client.models.generate_content(
+        model='gemini-3.5-flash',
+        contents='我想查詢靜宜大學資管系的評價？',
+    )
+
+    return response.text
 
 
 @app.route("/webhook", methods=["POST"])
